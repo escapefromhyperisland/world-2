fc91a6e090af3fc2494f89455e1b410d4469b48e
Rafe <rafenasir@gmail.com>
2021-06-08 17:13:40 +0200

    removed node modeules

--- level-6/src/App.js
+++ level-6/src/App.js
@@ -1,6 +1,7 @@
 import Level1 from "../src/components/Level1";
 import Level2 from "./components/Level2";
 import Congratulation from "./components/Congratulation";
+import FinalScreen from "./components/FinalScreen";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" exact component={Congratulation} />
           <Route path="/Level1" exact component={Level1} />
           <Route path="/level2" component={Level2} />
+          <Route path="/finalscreen" component={FinalScreen} />
         </Switch>
       </div>
     </Router>
---
+++ level-6/src/components/FinalScreen.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState } from "react";
+// import { Link } from "react-router-dom";
+import "./level2.css";
+
+function FinalScreen() {
+  return (
+    <div className="level2">
+      <section className="question">
+        <h1>You are ready to go back to your planet, have a nice journey!!</h1>
+      </section>
+    </div>
+  );
+}
+
+export default FinalScreen;
